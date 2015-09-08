8d91b5394084a2393ac0066854791717c21d3df9
Noel Delgado <auhcsei@gmail.com>
2015-09-08 17:08:35 -0500

    add / namespace

--- public/js/application.js
+++ public/js/application.js
@@ -3,3 +3,5 @@ require('./../css/style.css');
 require('neon');
 require('neon/stdlib');
 require('./lib/widget.js');
+
+require('./lib/namespace');
---
+++ public/js/lib/namespace.js
@@ -0,0 +1,3 @@
+window.EM = window.EM || {};
+window.EM.UI = window.EM.UI || {};
+window.EM.Views = window.EM.Views || {};
