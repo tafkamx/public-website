2febd87e34552a78a933bb2f7c057d24c30f03fb
Noel Delgado <auhcsei@gmail.com>
2015-09-16 13:32:53 -0500

    update bios

--- public/js/data/about-us/team.js
+++ public/js/data/about-us/team.js
@@ -9,7 +9,7 @@ module.exports = [
             {type : 'twitter', url : 'https://twitter.com/cuiki'},
             {type : 'medium', url : 'https://medium.com/@cuiki'}
         ],
-        bio : 'Darío is the founder and chief executive officer of Empathia. Everyone calls him Cuiki so you can go ahead and call him like that. For those of you wondering, no, there is no english origin behind his nickname.',
+        bio : 'Welcome! I am the founder and chief executive officer of Empathia. Everyone calls me Cuiki so go ahead and join the trend :P. For those of you wondering, no, there is no english origin behind my nickname. I am a designer studying the essentials of what makes us human. Psychology, linguistics, sociology, philosophy, among others. This way we can leverage our talents and inject back this humanity into what we do and help make the lives of people easier and better.'
     },
 
     {
@@ -22,7 +22,7 @@ module.exports = [
             {type : 'twitter', url : 'https://twitter.com/sgarza'},
             {type : 'soundcloud', url : 'soundcloud.com/sergio-de-la-garza-palacios'}
         ],
-        bio : 'Hi!, there are a lot of things I enjoy, but must of all I love my family, the work I do and music. I enjoy contributing to our city IT ecosystem, as a member of H/F ProDev I’m always looking for ways to help other people to improve their skills and to give them the tools and knowledge to be better engineers. I’m also a musician, some times I cover a song, some others I feel brave enough to make one of my own, so hop on my soundcloud and leave me a comment.',
+        bio : 'Hi! There’s a lot of things I enjoy, but must of all I love my family, the work I do and music. I enjoy contributing to our city’s IT ecosystem, as a member of H/F ProDev. I’m always looking for ways to help other people to improve their skills and to give them the tools and knowledge to be better engineers. I’m also a musician and sometimes I cover a song. Every once in a while I feel brave enough to make one of my own, so stop by my SoundCloud page and leave me a comment!'
     },
 
     {
@@ -35,10 +35,7 @@ module.exports = [
             {type : 'codepen', url : 'http://codepen.io/noeldelgado'},
             {type : 'twitter', url : 'https://twitter.com/pixelia_me'}
         ],
-        bio : 'Hi! I love creative coding, glitches, interactive things, animations, dead pixels, metal music & horror stuff.' +
-            ' I enjoy teaching people, learning and building new things.' +
-            ' I also like to draw and illustrate from time to time.' +
-            ' Psst, I have to admit that I’ve been caught listening to 90’s pop and dance music *secretly*, maybe childhood memories?, we may never know. ;)'
+        bio : 'I love creative coding, glitches, interactive things, animations, dead pixels, metal music & horror stuff. Teaching people, learning and building new things is what makes me wake up every morning. I really enjoy drawing and illustrate from time to time. Psst, I have to admit that I’ve been caught listening to 90’s pop and dance music *secretly*. Maybe childhood memories? We may never know.'
     },
 
     {
@@ -63,7 +60,7 @@ module.exports = [
             {type : 'twitter', url : 'https://twitter.com/Ericku_'},
             {type : 'steam', url : 'http://steamcommunity.com/profiles/76561198060717183/'}
         ],
-        bio : 'Hey there, I enjoy traveling, listening to weird music & in general trying new things. I’m fascinated by the ways people around the world solves the same problems in different ways. As a designer I strive to make this world better, cheesy , but i do believe it is possible (:'
+        bio : 'Hey there! I enjoy traveling, listening to weird music & in general trying new things. I’m fascinated by the way people around the world solve the same problems in many different ways. With that inspiration and as a designer, I strive to make this world better. Cheesy? Yeah probably, but I do believe it is possible. (:'
     },
 
     {
@@ -74,7 +71,7 @@ module.exports = [
         networks : [
             {type : 'twitter', url : 'https://twitter.com/albertovazquezz'}
         ],
-        bio : ''
+        bio : 'Hello I’m Beto. I’m currently studying industrial engineering. I enjoy spending time riding my bike and discovering new landscapes. I love tacos & sushi, jazz & mariachi and Ben & Jerry’s. I read the newspaper everyday because I think information is key and essential to understand your surroundings. Collaboration is the first solution for the betterment of communities and my passion is to bring the concept of teamwork to my neighbourhood’s meetings and coexist in a better place.'
     },
 
     {
@@ -87,6 +84,6 @@ module.exports = [
             {type : 'twitter', url : 'https://twitter.com/EduanGreduan'},
             {type : 'steam', url : 'http://steamcommunity.com/id/greduan'}
         ],
-        bio : 'Hi!, I enjoy programming, bicycling, playing video games, eating, whistling and humming, and just in general interacting with other people. I enjoy helping people with my work so all of my personal projects are open source. Homeschooled, so although I’ve been living in a cave for the last 18 years, I am discovering the hidden intricacies of this society’s social interactions. Getting there…'
+        bio : 'Hi! I enjoy programming, bicycling, playing video games, eating, whistling and humming (yeah, I know it can get annoying) and just in general interacting with other people. I enjoy helping people with my work so all of my personal projects are open source. Homeschooled, so although I’ve been living in a cave for the last 18 years, I am discovering the hidden intricacies of this society’s social interactions. Getting there…'
     }
 ];
