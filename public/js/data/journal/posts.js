c744acce2c3455b83c86b2ce310e217a357dbe0e
Noel Delgado <auhcsei@gmail.com>
2016-05-31 19:14:02 -0500

    update / journal posts

--- public/js/data/journal/posts.js
+++ public/js/data/journal/posts.js
@@ -1,10 +1,34 @@
 module.exports = [
-    {
-        data : {
-            title : 'Let’s start with the why.',
-            category : 'Misc',
-            by : 'Dario',
-            url : 'https://medium.com/@cuiki/let-s-start-with-the-why-b3a2b444cbd5#.4ds30otru'
-        }
-    },
+  {
+    data: {
+      title: 'Why we built our own backend framework.',
+      category: 'Misc',
+      by: 'Checo',
+      url: 'https://medium.empathia.agency/why-we-built-our-own-backend-framework-c91e11f854c4#.7mz9t5s1k'
+    }
+  },
+  {
+    data: {
+      title: 'Just a quick thought… about our biggest challenge.',
+      category: 'Misc',
+      by: 'Dario',
+      url: 'https://medium.empathia.agency/just-a-quick-thought-about-our-biggest-challenge-e70ec9c65b82#.advyt6e6k'
+    }
+  },
+  {
+    data: {
+      title: 'Thoughts and lessons about breakups.',
+      category: 'Misc',
+      by: 'Dario',
+      url: 'https://medium.empathia.agency/thoughts-and-lessons-about-breakups-d920ff3e0071#.dyplxo7tq'
+    }
+  },
+  {
+    data : {
+      title : 'Let’s start with the why.',
+      category : 'Misc',
+      by : 'Dario',
+      url : 'https://medium.com/@cuiki/let-s-start-with-the-why-b3a2b444cbd5#.4ds30otru'
+    }
+  },
 ];
